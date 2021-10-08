733989f00a896060e0ef30fa62311150c541bae3
Karim <karimhamid@yandex.ru>
2021-10-08 21:53:22 +0300

    .

--- hackathon_travelers/settings.py
+++ hackathon_travelers/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [STATIC_DIR]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- main/views.py
+++ main/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView, View
 
-# Create your views here.
+
+class MainView(TemplateView):
+    template_name = 'index.html'
